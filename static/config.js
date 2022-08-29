86113f203509548049b710d823e6518b0195f413
bobpepers <bob.pepers@gmail.com>
2022-08-29 14:57:15 +0200

    fix: 🐛 add missing var config.js

--- static/config.js
+++ static/config.js
@@ -1,10 +1,12 @@
 const setConfigDevelopment = () => {
+  window.myConfig = {};
   window.myConfig.apiUrl = 'https://localhost/api';
   window.myConfig.wsEndPoint = 'https://localhost';
   window.myConfig.reCaptchaSiteKey = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
 }
 
 const setConfigProduction = () => {
+  window.myConfig = {};
   window.myConfig.apiUrl = 'https://bots.runebase.io/api';
   window.myConfig.wsEndPoint = 'https://bots.runebase.io';
   window.myConfig.reCaptchaSiteKey = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
