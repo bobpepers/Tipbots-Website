c9dea01059f0ecf9d3fcab88af07aa32a219b34b
bobpepers <bob.pepers@gmail.com>
2022-10-10 04:27:15 +0200

    feat: ✨ Add StellarTip to website

--- src/app/helpers/tipbotsInfoArray.js
+++ src/app/helpers/tipbotsInfoArray.js
@@ -2,6 +2,7 @@ import RunesCoin from '../assets/images/runescoin.png';
 import Pirate from '../assets/images/pirate.png';
 import Tokel from '../assets/images/tokel.png';
 import Secret from '../assets/images/secret.png';
+import Stellar from '../assets/images/stellar.png';
 
 export const tipbotInfoArray = [
   {
@@ -16,6 +17,7 @@ export const tipbotInfoArray = [
     telegramCommandPrefix: '!runestip',
     uptimeRobotMonitorId: '791625646',
     exampleAddress: 'ReU2nhYXamYRd2VBk4auwresov6jwLEuSg',
+    multiToken: false,
   },
   {
     name: 'PirateTip',
@@ -29,6 +31,7 @@ export const tipbotInfoArray = [
     telegramCommandPrefix: '!piratetip',
     uptimeRobotMonitorId: '791626722',
     exampleAddress: 'zs1e3zh7a00wz4ej2lacpl2fvnrl680hkk766nt7z4ujl6rlj04n59ex7hjlnknvhwdc7vxzn0kcvt',
+    multiToken: false,
   },
   {
     name: 'TokelTip',
@@ -49,11 +52,29 @@ export const tipbotInfoArray = [
     coin: 'Secret Network',
     logo: Secret,
     explorer: 'https://www.mintscan.io/secret',
-    discordLink: 'https://discord.com/api/oauth2/authorize?client_id=1017103311570223144&permissions=523328&scope=bot%20applications.commands',
+    discordLink: 'https://discord.com/api/oauth2/authorize?client_id=1024632097199108138&permissions=523328&scope=bot%20applications.commands',
     telegramLink: 'https://t.me/SecretTip_bot',
     discordCommandPrefix: '/secrettip',
     telegramCommandPrefix: '!secrettip',
     uptimeRobotMonitorId: '792646669',
     exampleAddress: 'secret1fsw6l4nkvy3pnjynnqyehdyhvh26qk4y9hvnru',
+    multiToken: false,
+  },
+  {
+    name: 'StellarTip',
+    ticker: 'XLM',
+    coin: 'Stellar Lumens',
+    logo: Stellar,
+    explorer: 'https://stellarchain.io/',
+    discordLink: 'https://discord.com/api/oauth2/authorize?client_id=1017103311570223144&permissions=523328&scope=bot%20applications.commands',
+    telegramLink: 'https://t.me/stellartip_bot',
+    discordCommandPrefix: '/stellartip',
+    telegramCommandPrefix: '!stellartip',
+    uptimeRobotMonitorId: '792830503',
+    exampleAddress: 'GBBZSVVHGE2RV642Z6BYO6EU4RZVAC56D2YHJDE7AZIU6LZSRYBVRWAM',
+    multiToken: true,
+    tokens: [
+      'DXLM',
+    ],
   },
 ]
