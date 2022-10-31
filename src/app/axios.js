f8a35de47a3ca6fc81c1fd1b4e112488f98ebbf9
bobpepers <bob.pepers@gmail.com>
2022-10-31 09:41:36 +0100

    chore(release): 0.0.32

--- src/app/axios.js
+++ src/app/axios.js
@@ -4,6 +4,6 @@ axios.defaults.withCredentials = true;
 axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
 axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
 // axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'discordapp.com';
-axios.defaults.headers.get['Access-Control-Allow-Credentials'] = true;
+// axios.defaults.headers.get['Access-Control-Allow-Credentials'] = true;
 
 export default axios;
