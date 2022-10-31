1df063cf0165e18272b434418cf286cf758d8934
bobpepers <bob.pepers@gmail.com>
2022-10-31 04:09:14 +0100

    chore(release): 0.0.29

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [0.0.29](https://github.com/bobpepers/Tipbots-Website/compare/v0.0.28...v0.0.29) (2022-10-31)
+
 ### [0.0.28](https://github.com/bobpepers/Tipbots-Website/compare/v0.0.27...v0.0.28) (2022-10-31)
 
 
--- src/app/axios.js
+++ src/app/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
 axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'discordapp.com';
+// axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'discordapp.com';
+axios.defaults.headers.get['Access-Control-Allow-Credentials'] = true;
 
 export default axios;
